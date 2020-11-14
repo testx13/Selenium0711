1997ff3df05da72c2a54c3828557ceb6dfaa6fc7
testx13 <74459746+testx13@users.noreply.github.com>
2020-11-14 07:19:13 -0800

    Update test.java
    since GIT

--- Selenium0711/src/main/java/test.java
+++ Selenium0711/src/main/java/test.java
@@ -8,7 +8,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TEST");
+		System.out.println("TESTGIT");
 		String exePath= Paths.get("").toAbsolutePath().toString()+File.separator+"drivers"+ File.separator; // Seteando el path de nuestro driver
 		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //Identificador el .exe para driver
 		WebDriver driver=new ChromeDriver(); // Creacion en tiempo de ejecucion
