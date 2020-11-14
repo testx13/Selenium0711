d0d69c698318c1d20a030f0fd1daacee4c205fc2
Zajar <buhitox@hotmail.com>
2020-11-14 07:28:48 -0800

    Revert "Update test.java"
    This reverts commit 1997ff3df05da72c2a54c3828557ceb6dfaa6fc7.

--- Selenium0711/src/main/java/test.java
+++ Selenium0711/src/main/java/test.java
@@ -8,7 +8,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TESTGIT");
+		System.out.println("TEST");
 		String exePath= Paths.get("").toAbsolutePath().toString()+File.separator+"drivers"+ File.separator; // Seteando el path de nuestro driver
 		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //Identificador el .exe para driver
 		WebDriver driver=new ChromeDriver(); // Creacion en tiempo de ejecucion
