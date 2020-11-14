d281074afc8b0a0a1e478f06a5ad2b415f43fbff
Zajar <buhitox@hotmail.com>
2020-11-14 06:49:48 -0800

    firts commit

---
+++ Selenium0711/src/main/java/test.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST");
+		String exePath= Paths.get("").toAbsolutePath().toString()+File.separator+"drivers"+ File.separator; // Seteando el path de nuestro driver
+		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe"); //Identificador el .exe para driver
+		WebDriver driver=new ChromeDriver(); // Creacion en tiempo de ejecucion
+		driver.get("https://www.google.com");
+		
+		
+	}
+
+}
---
+++ Selenium0711/src/test/java/TestNG/Test1.java
@@ -0,0 +1,11 @@
+package TestNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() {
+	  Assert.assertTrue(true);
+  }
+}
